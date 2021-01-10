5b99f092ff098f39fe54868e0a46c71b93bfc0b1
J. Cohen <hostelmarketing@gmail.com>
2021-01-09 20:01:57 -0800

    Add some user interaction

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import           StrAdd
+
 main :: IO ()
-main = undefined
+main = do
+    putStrLn "enter first argument"
+    a <- getLine
+    putStrLn "enter second argument"
+    b <- getLine
+
+    let result = strAddInts a b
+
+    putStrLn (displayResult result)
--- src/StrAdd.hs
+++ src/StrAdd.hs
@@ -1,7 +1,22 @@
-module StrAdd where
+module StrAdd
+    ( isAllDigits
+    , strAddInts
+    , displayResult
+    ) where
 
 import           Data.Char
 
 isAllDigits :: String -> Bool
 isAllDigits "" = False
 isAllDigits xs = all ((== True) . isDigit) xs
+
+strAddInts :: String -> String -> Either String Int
+strAddInts a b
+    | isAllDigits a && isAllDigits b       = Right (read a + read b)
+    | not (isAllDigits a || isAllDigits b) = Left "both args are wrong"
+    | not (isAllDigits a)                  = Left "first arg is wrong"
+    | otherwise                            = Left "second arg is wrong"
+
+displayResult :: Either String Int -> String
+displayResult (Left  addError) = "error: " ++ show addError
+displayResult (Right res     ) = "answer: " ++ show res
